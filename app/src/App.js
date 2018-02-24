c1531c1d5963dc6265c27e02074b798c36cc1bee
Leighton Lilford <lrlilford@gmail.com>
2018-02-24 18:49:48 +0800

    Finish Profiles

--- app/src/App.js
+++ app/src/App.js
@@ -17,9 +17,29 @@ const { Header, Content, Footer } = Layout;
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.setPage = this.setPage.bind(this);
+    this.getPage = this.getPage.bind(this);
+
+    this.state = {page: "1"};
   }
 
+  setPage = ({key}) => {
+    this.setState({page: key});
+  }
+  getPage() {
+    const {page} = this.state
+    switch (page) {
+      case "6":
+        return (<Profile />)
+      default:
+        return "Page here";
+    }
+  }
   render() {
+    const {page} = this.state;
+    const getPage = this.getPage;
+
     return (
       <div className="App">
         <BackTop />
@@ -28,12 +48,13 @@ class App extends Component {
             <Menu
               theme="dark"
               mode="horizontal"
-              defaultSelectedKeys={["1"]}
+              selectedKeys={[String(page)]}
               style={{ lineHeight: "64px" }}
+              onClick={this.setPage}
             >
               {_.map(menu, i => {
                 return (
-                  <Menu.Item key={i.id}>
+                  <Menu.Item key={i.id} >
                     <Icon type={i.icon} />
                     <span>{i.text}</span>
                   </Menu.Item>
@@ -42,7 +63,7 @@ class App extends Component {
             </Menu>
           </Header>
           <Content style={{ padding: "20px 50px" }}>
-            <Profile />
+            {getPage()}
           </Content>
           <Footer style={{ textAlign: "center" }}>
             Leighton Lilford - Last updated 21 Feb 2018
--- app/src/Pages/profile/index.js
+++ app/src/Pages/profile/index.js
@@ -1,23 +1,21 @@
 import React, { Component } from "react";
 import _ from "lodash";
 import * as consts from "./consts";
-import { Row, Col, Card, Icon, Avatar } from "antd";
+import { Row, Col, Card } from "antd";
 
 const { Meta } = Card;
 
 class Profile extends Component {
-  constructor(props) {
-    super(props);
-  }
 
   render() {
     return (
       <div style={{ background: "#ECECEC", padding: "30px" }}>
+      <h2>Web Profiles</h2>
         <Row gutter={16}>
           {_.map(consts.profiles, p => {
             return (
-              <Col span={5}>
-                <a href={p.url} key={p.url}>
+              <Col span={6}>
+                <a s href={p.url} key={p.url}>
                   <Card>
                     <Meta
                       avatar={p.avatar}
