99f12f817ebda6629f075f6820a7b2a65ed5c208
Leighton Lilford <lrlilford@gmail.com>
2023-01-15 14:41:42 +0800

    Move improvements

--- src/src/terminal/controller.tsx
+++ src/src/terminal/controller.tsx
@@ -1,15 +1,63 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import TerminalUI from "./ui";
 
-const InitialText = [
+const InitialText: string[] = [
   "Welcome!",
-  'Type "help" for a list of available commands - or click one of the buttons below',
+  'Type type or click one of the buttons below',
+];
+
+const commandMap = [
+  {
+    key: "about",
+    value: "todo",
+  },
+  {
+    key: "education",
+    value: "todo",
+  },
+  {
+    key: "skills",
+    value: "todo",
+  },
+  {
+    key: "experience",
+    value: "todo",
+  },
+  {
+    key: "interests",
+    value: "todo",
+  },
+  {
+    key: "contact",
+    value: "todo",
+  },
 ];
 function Terminal() {
   const [terminalContent, setTerminalContent] = useState(InitialText);
 
-  const onEnter = (text: String) => {};
-  return <TerminalUI terminalContent={terminalContent} onEnter={onEnter} />;
+  const onEnter = (text: string) => {
+    const response = commandMap.find((c) => c.key === text);
+    if (response) {
+      setTerminalContent([...terminalContent, response.key, response.value]);
+    } else {
+      setTerminalContent([...terminalContent, text, `Unknown command ${text}`]);
+    }
+  };
+  const validCommands = useMemo(
+    () =>
+      commandMap.reduce((c, a) => {
+        c.push(a.key);
+        return c;
+      }, [] as string[]),
+    []
+  );
+  return (
+    <TerminalUI
+      validCommands={validCommands}
+      terminalContent={terminalContent}
+      onEnter={onEnter}
+    />
+  );
 }
 
 export default Terminal;
--- src/src/terminal/ui.tsx
+++ src/src/terminal/ui.tsx
@@ -3,34 +3,29 @@ import { Keyframes, Frame } from "./react-keyframe";
 import "./ui.css";
 
 type TerminalProps = {
-  terminalContent: String[];
+  terminalContent: string[];
   onEnter: (t: string) => void;
+  validCommands: string[];
 };
 
 const sleepDuration = 300;
 const getTypingDuration = () => 40 + 80 * (Math.random() - 0.5);
 
 const renderText = (
-  text: String,
-  caret: { blinking: boolean } | false = false
+  text: string,
+  caret: { blinking: boolean } | false = false,
+  style: "valid" | "invalid" | "none" = "none"
 ) => (
   <>
     <span style={{ color: "#00c200" }}>{"~ "}</span>
     <span>{"$ "}</span>
-    <span>{text}</span>
-    {caret && (
-      <span
-        className={`caret ${caret.blinking && "blinking"}`}
-      />
-    )}
+    <span className={style}>{text}</span>
+    {caret && <span className={`caret ${caret.blinking && "blinking"}`} />}
   </>
 );
 
-const renderLine = (text: String, key: number, onComplete: () => void) => {
+const renderLine = (text: string, key: number, onComplete: () => void) => {
   const frames = [];
-
-  // starting frame
-  frames.push(<Frame duration={sleepDuration} key={`${text}-first`}></Frame>);
 
   // typing out the line
   for (let i = 0; i < text.length; i++) {
@@ -45,9 +40,7 @@ const renderLine = (text: String, key: number, onComplete: () => void) => {
 
   // ending frame
   frames.push(
-    <Frame key={`${text}-last`}>
-      {renderText(text, { blinking: false })}
-    </Frame>
+    <Frame key={`${text}-last`}>{renderText(text, { blinking: false })}</Frame>
   );
 
   return (
@@ -66,12 +59,25 @@ function Terminal(props: TerminalProps) {
 
   const focusInput = () => {
     inputRef.current?.focus();
-    resetCaret()
+    resetCaret();
   };
   const resetCaret = () => {
     // We want to disable moving of the caret in our hidden input
-    inputRef.current?.setSelectionRange(9999, 9999)
+    inputRef.current?.setSelectionRange(9999, 9999);
   };
+  const inputKeyPress: React.KeyboardEventHandler<HTMLInputElement> = (key) => {
+    resetCaret();
+    if (key.key === "Enter") {
+      inputRef.current?.value && submit(inputRef.current!.value);
+    }
+  };
+
+  const submit = (text: string) => {
+    setCurrentText("");
+    props.onEnter(text);
+  };
+
+  const waitingOnPrompt = currentLine === terminalContent.length;
 
   return (
     <div className="terminalWrapper">
@@ -84,25 +90,46 @@ function Terminal(props: TerminalProps) {
         {terminalContent.map((text, index) => {
           if (index < currentLine) {
             return <p key={index}>{renderText(text)}</p>;
-          } else if (index == currentLine) {
+          } else if (index === currentLine) {
             return renderLine(text, index, () => setCurrentLine((i) => i + 1));
           }
+          return null;
         })}
-        {currentLine === terminalContent.length && (
+        <>
           <p style={{ cursor: "text" }}>
-            {renderText(currentText, { blinking: true })}
-            <div className="hidden">
-              <input
-                ref={inputRef}
-                autoFocus
-                onKeyUp={resetCaret}
-                onKeyDown={resetCaret}
-                onChange={(e) => {
-                  setCurrentText(e.target.value);
-                }}
-              />
-            </div>
+            {renderText(
+              currentText,
+              waitingOnPrompt ? { blinking: true } : false,
+              props.validCommands.includes(currentText) ? "valid" : "invalid"
+            )}
           </p>
+          <div className="hidden">
+            <input
+              ref={inputRef}
+              autoFocus
+              value={currentText}
+              onKeyUp={inputKeyPress}
+              onKeyDown={resetCaret}
+              onChange={(e) => {
+                if (waitingOnPrompt) {
+                  setCurrentText(e.target.value);
+                }
+              }}
+            />
+          </div>
+        </>
+        {waitingOnPrompt && (
+          <div className="bottomBar">
+            {props.validCommands.map((c) => (
+              <button
+                onClick={() => submit(c)}
+                key={c}
+                className="bottomButton"
+              >
+                {c}
+              </button>
+            ))}
+          </div>
         )}
       </div>
     </div>
